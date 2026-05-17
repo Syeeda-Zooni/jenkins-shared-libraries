d2fe5ead1357d459910ba77bf660431cc590a8ec
Syeeda-Zooni <syeedazooni22@gmail.com>
2026-05-17 11:18:06 +0530

    Rename code_build function to call

--- vars/code_build.groovy
+++ vars/code_build.groovy
@@ -1,3 +1,3 @@
-def code_build(String DockerHubUser, String ProjectName, String ImageTag){
+def call(String DockerHubUser, String ProjectName, String ImageTag){
   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
